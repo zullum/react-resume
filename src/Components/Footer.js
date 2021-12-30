501a8c73725bb7a6e2f944c20980720d5029da22
zullum <zullumsan@gmail.com>
2021-12-30 17:04:48 +0100

    Fixed footer copyright.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              <a title="zullum" href="https://sanelzulic.netlify.app/">
                 PAPA
               </a>
             </li>
